de7b3327a8bf7c7e6e9671a77abd83174114d26f
sumi <sumipavib1302@gmail.com>
2023-01-24 21:02:20 +0530

    Added:insert 30 between 70 & 56

--- src/com/linkedlist/LinkedList.java
+++ src/com/linkedlist/LinkedList.java
@@ -15,6 +15,7 @@ public class LinkedList<T> {
 	        }
 
 	    }
+	    
 	    public void display() {
 	        Node<T> temp = head;
 	        while (temp != null) {
@@ -24,16 +25,31 @@ public class LinkedList<T> {
 	        System.out.println();
 	    }
 	    
-	    public void append(){
-	        T key = null;
-	        Node<T> newNode = new Node<>(key);
-	        if (head == null){
-	            head=newNode;
-	            tail= newNode;
-	        }else {
-	            tail.next = newNode;
+	    //insert elements between elements in a linkedlist
+	    public void insert(T key,int position){
+	        //Create a new node
+	        Node<T> newNode = new Node(key);
+	        if(head == null) {
+	            //If list is empty, both head and tail would point to new node
+	            head = newNode;
 	            tail = newNode;
 	        }
+	        else {
+	            Node temp, current;
+	            temp = head;
+	            current = null;
+	            for(int i = 0; i < position; i++) {
+	                //Node current will point to temp
+	                current = temp;
+	                //Node temp will point to node next to it.
+	                temp = temp.next;
+	            }
+	            //current will point to new node
+	            current.next = newNode;
+	            //new node will point to temp
+	            newNode.next = temp;
+	        }
+
 	    }
 	    
 	    
--- src/com/linkedlist/LinkedListMain.java
+++ src/com/linkedlist/LinkedListMain.java
@@ -5,16 +5,11 @@ public class LinkedListMain {
 	public static void main(String[] args) {
         LinkedList<Integer> linkedList1 = new LinkedList<>();
         linkedList1.add(70);
-        linkedList1.add(30);
         linkedList1.add(56);
         linkedList1.display();
         
-        LinkedList<Integer> linkedList2 = new LinkedList<>();
-        linkedList2.add(56);
-        linkedList2.add(30);
-        linkedList2.add(70);
-        linkedList2.display();
-        
+        linkedList1.insert(30,1);
+        linkedList1.display();
       
     
     }
